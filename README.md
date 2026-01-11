4f57c0b41e6ee845dd0bd9aba2050b2efc915415
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-01-11 17:36:13 +0300

    prj - Prepared the Calendrier project
    ---
    
    Type: prj
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ README.md
@@ -0,0 +1,53 @@
+
+<div align = center>
+
+<br>
+<br>
+    
+<img
+  src = 'https://cdn.jsdelivr.net/gh/Aptivi/Calendrier@main/assets/OfficialAppIcon-Calendrier-512.png'
+  width = 256
+  align = center
+/>
+
+<br>
+
+# Calendrier
+    
+*Calendar tools library for C# CLI, TUI, and GUI applications*
+
+<br>
+<br>
+
+[![Badge NuGet]][NuGet]
+
+![Badge Size]   
+[![Badge Downloads]][Releases]
+
+[![Button Manual]][Manual]   
+
+</div>
+    
+<br>
+
+</div>
+
+
+<!----------------------------------------------------------------------------->
+
+[Releases]: https://github.com/Aptivi/Calendrier/releases
+[NuGet]: https://www.nuget.org/packages/Calendrier/
+
+[Manual]: https://aptivi.gitbook.io/calendrier-manual/
+
+<!----------------------------------[ Badges ]--------------------------------->
+
+[Badge Downloads]: https://img.shields.io/github/downloads/Aptivi/Calendrier/total?color=217346&label=Downloads&style=for-the-badge&logoColor=white&logo=DocuSign&labelColor=2d9d5f
+[Badge NuGet]: https://img.shields.io/nuget/vpre/Calendrier?color=012f52&style=for-the-badge&logoColor=white&logo=NuGet&labelColor=004880
+[Badge Size]: https://img.shields.io/github/repo-size/Aptivi/Calendrier?color=bb4a28&label=size&logoColor=white&style=for-the-badge&logo=GoogleAnalytics&labelColor=E85C33
+
+
+<!---------------------------------[ Buttons ]--------------------------------->
+
+[Button Libraries]: https://img.shields.io/badge/Libraries-EA8220?style=for-the-badge&logoColor=white&logo=AzureArtifacts
+[Button Manual]: https://img.shields.io/badge/Docs-blueviolet?style=for-the-badge&logoColor=white&logo=GitBook
---
+++ private/Calendrier.Tests/CalendarTests.cs
@@ -0,0 +1,111 @@
+﻿//
+// Calendrier  Copyright (C) 2026  Aptivi
+//
+// This file is part of Calendrier
+//
+// Calendrier is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Calendrier is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Calendrier.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Calendrier.Tests
+{
+    [TestClass]
+    public sealed class CalendarTests
+    {
+        [TestMethod]
+        [DataRow(nameof(CalendarTypes.Chinese), nameof(ChineseCalendar))]
+        [DataRow(nameof(CalendarTypes.Gregorian), nameof(GregorianCalendar))]
+        [DataRow(nameof(CalendarTypes.Hijri), nameof(HijriCalendar))]
+        [DataRow(nameof(CalendarTypes.Japanese), nameof(JapaneseCalendar))]
+        [DataRow(nameof(CalendarTypes.Persian), nameof(PersianCalendar))]
+        [DataRow(nameof(CalendarTypes.SaudiHijri), nameof(SaudiHijriCalendar))]
+        [DataRow(nameof(CalendarTypes.Taiwanese), nameof(TaiwaneseCalendar))]
+        [DataRow(nameof(CalendarTypes.ThaiBuddhist), nameof(ThaiBuddhistCalendar))]
+        [DataRow(nameof(CalendarTypes.Variant), nameof(VariantCalendar))]
+        public void TestGetCalendarFromName(string calendarName, string expectedCalendarNameType)
+        {
+            var calendar = CalendarTools.GetCalendar(calendarName);
+            calendar.GetType().Name.ShouldBe(expectedCalendarNameType);
+        }
+
+        [TestMethod]
+        [DataRow(nameof(CalendarTypes.Chinese), nameof(ChineseCalendar), "zh-CN", "GregorianCalendar")]
+        [DataRow(nameof(CalendarTypes.Gregorian), nameof(GregorianCalendar), "en-US", "GregorianCalendar")]
+        [DataRow(nameof(CalendarTypes.Hijri), nameof(HijriCalendar), "ar", "HijriCalendar")]
+        [DataRow(nameof(CalendarTypes.Japanese), nameof(JapaneseCalendar), "ja-JP", "JapaneseCalendar")]
+        [DataRow(nameof(CalendarTypes.Persian), nameof(PersianCalendar), "fa", "PersianCalendar")]
+        [DataRow(nameof(CalendarTypes.SaudiHijri), nameof(SaudiHijriCalendar), "ar-SA", "UmAlQuraCalendar")]
+        [DataRow(nameof(CalendarTypes.Taiwanese), nameof(TaiwaneseCalendar), "zh-TW", "TaiwanCalendar")]
+        [DataRow(nameof(CalendarTypes.ThaiBuddhist), nameof(ThaiBuddhistCalendar), "th-TH", "ThaiBuddhistCalendar")]
+        public void TestGetCalendarFromNameDeep(string calendarName, string expectedCalendarNameType, string expectedCulture, string expectedCalendarType)
+        {
+            var calendar = CalendarTools.GetCalendar(calendarName);
+            calendar.GetType().Name.ShouldBe(expectedCalendarNameType);
+            calendar.Culture.Name.ShouldBe(expectedCulture);
+            calendar.Calendar.GetType().Name.ShouldBe(expectedCalendarType);
+        }
+
+        [TestMethod]
+        [DataRow(CalendarTypes.Chinese, nameof(ChineseCalendar))]
+        [DataRow(CalendarTypes.Gregorian, nameof(GregorianCalendar))]
+        [DataRow(CalendarTypes.Hijri, nameof(HijriCalendar))]
+        [DataRow(CalendarTypes.Japanese, nameof(JapaneseCalendar))]
+        [DataRow(CalendarTypes.Persian, nameof(PersianCalendar))]
+        [DataRow(CalendarTypes.SaudiHijri, nameof(SaudiHijriCalendar))]
+        [DataRow(CalendarTypes.Taiwanese, nameof(TaiwaneseCalendar))]
+        [DataRow(CalendarTypes.ThaiBuddhist, nameof(ThaiBuddhistCalendar))]
+        [DataRow(CalendarTypes.Variant, nameof(VariantCalendar))]
+        public void TestGetCalendarFromEnum(CalendarTypes calendarName, string expectedCalendarNameType)
+        {
+            var calendar = CalendarTools.GetCalendar(calendarName);
+            calendar.GetType().Name.ShouldBe(expectedCalendarNameType);
+        }
+
+        [TestMethod]
+        [DataRow(CalendarTypes.Chinese, nameof(ChineseCalendar), "zh-CN", "GregorianCalendar")]
+        [DataRow(CalendarTypes.Gregorian, nameof(GregorianCalendar), "en-US", "GregorianCalendar")]
+        [DataRow(CalendarTypes.Hijri, nameof(HijriCalendar), "ar", "HijriCalendar")]
+        [DataRow(CalendarTypes.Japanese, nameof(JapaneseCalendar), "ja-JP", "JapaneseCalendar")]
+        [DataRow(CalendarTypes.Persian, nameof(PersianCalendar), "fa", "PersianCalendar")]
+        [DataRow(CalendarTypes.SaudiHijri, nameof(SaudiHijriCalendar), "ar-SA", "UmAlQuraCalendar")]
+        [DataRow(CalendarTypes.Taiwanese, nameof(TaiwaneseCalendar), "zh-TW", "TaiwanCalendar")]
+        [DataRow(CalendarTypes.ThaiBuddhist, nameof(ThaiBuddhistCalendar), "th-TH", "ThaiBuddhistCalendar")]
+        public void TestGetCalendarFromEnumDeep(CalendarTypes calendarName, string expectedCalendarNameType, string expectedCulture, string expectedCalendarType)
+        {
+            var calendar = CalendarTools.GetCalendar(calendarName);
+            calendar.GetType().Name.ShouldBe(expectedCalendarNameType);
+            calendar.Culture.Name.ShouldBe(expectedCulture);
+            calendar.Calendar.GetType().Name.ShouldBe(expectedCalendarType);
+        }
+
+        [TestMethod]
+        [DataRow(nameof(CalendarTypes.Chinese), CalendarTypes.Chinese)]
+        [DataRow(nameof(CalendarTypes.Gregorian), CalendarTypes.Gregorian)]
+        [DataRow(nameof(CalendarTypes.Hijri), CalendarTypes.Hijri)]
+        [DataRow(nameof(CalendarTypes.Japanese), CalendarTypes.Japanese)]
+        [DataRow(nameof(CalendarTypes.Persian), CalendarTypes.Persian)]
+        [DataRow(nameof(CalendarTypes.SaudiHijri), CalendarTypes.SaudiHijri)]
+        [DataRow(nameof(CalendarTypes.Taiwanese), CalendarTypes.Taiwanese)]
+        [DataRow(nameof(CalendarTypes.ThaiBuddhist), CalendarTypes.ThaiBuddhist)]
+        [DataRow(nameof(CalendarTypes.Variant), CalendarTypes.Variant)]
+        public void TestGetCalendarTypeFromName(string calendarName, CalendarTypes expectedCalendarType)
+        {
+            var calendar = CalendarTools.GetCalendarTypeFromName(calendarName);
+            calendar.ShouldBe(expectedCalendarType);
+        }
+    }
+}
